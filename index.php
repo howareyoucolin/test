b1d17c553e684e4be6226ef14440ffb5feadd836
howareyoucolin <howareyoucolin@gmail.com>
2019-08-27 14:54:46 -0400

    chaneg

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php 
   $data = "<soap:Envelope>[...]</soap:Envelope>"; 
 $tuCurl = curl_init(); 
-curl_setopt($tuCurl, CURLOPT_URL, "https://example.com/path/for/soap/url/"); 
-curl_setopt($tuCurl, CURLOPT_PORT , 443); 
+       curl_setopt($tuCurl, CURLOPT_URL, "https://example.com/path/for/soap/url/"); 
+curl_setopt(
+  $tuCurl, CURLOPT_PORT , 443); 
 curl_setopt($tuCurl, CURLOPT_VERBOSE, 0); 
 curl_setopt($tuCurl, CURLOPT_HEADER, 0); 
 curl_setopt($tuCurl, CURLOPT_SSLVERSION, 3); 
 curl_setopt($tuCurl, CURLOPT_SSLCERT, getcwd() . "/client.pem"); 
-curl_setopt($tuCurl, CURLOPT_SSLKEY, getcwd() . "/keyout.pem"); 
+curl_setopt($tuCurl,
+            CURLOPT_SSLKEY, getcwd() . "/keyout.pem"); 
 curl_setopt($tuCurl, CURLOPT_CAINFO, getcwd() . "/ca.pem"); 
 curl_setopt($tuCurl, CURLOPT_POST, 1); 
 curl_setopt($tuCurl, CURLOPT_SSL_VERIFYPEER, 1); 
@@ -16,7 +18,8 @@ curl_setopt($tuCurl, CURLOPT_POSTFIELDS, $data);
 curl_setopt($tuCurl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml","SOAPAction: \"/soap/action/query\"", "Content-length: ".strlen($data))); 
 
 $tuData = curl_exec($tuCurl); 
-if(!curl_errno($tuCurl)){ 
+if(!curl_errno($tuCurl))
+{ 
   $info = curl_getinfo($tuCurl); 
   echo 'Took ' . $info['total_time'] . ' seconds to send a request to ' . $info['url']; 
 } else { 
